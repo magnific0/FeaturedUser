a422d5932d93ada2c4cd78071e497107e4721cb7
Jacco Geul <jacco@geul.net>
2015-11-06 09:48:27 +0100

    Add modular approach for bits (not only about).

--- FeaturedUser.class.php
+++ FeaturedUser.class.php
@@ -1,21 +1,21 @@
 <?php
 /**
  * FeaturedUser extension - adds <featureduser> parser hook
- * to display a featuredly chosen 'featured' user and some info regarding the
- * user, such as their avatar.
+ * to display a chosen user and some info regarding the
+ * user, such as their avatar, bio etc.
  *
  * Meant to be used with the SocialProfile extension, fails without.
- * Make sure to set either $wgUserStatsTrackWeekly or $wgUserStatsTrackMonthly
- * to true in your wiki's LocalSettings.php and before doing so, be sure that
- * you have created the three necessary tables in the database:
- * user_points_archive, user_points_monthly and user_points_weekly.
- * Then add <featureduser/> tag to whichever page you want to.
+ * Add <featureduser user="User Name"/> tag to display an user on
+ * any page.
+ *
+ * Heavily inspired by RandomFeaturedUser extension:
+ * https://www.mediawiki.org/wiki/Extension:RandomFeaturedUser
+ * Aaron Wright <aaron.wright@gmail.com>, David Pean <david.pean@gmail.com>
+ * and Jack Phoenix <jack@countervandalism.net>.
  *
  * @file
  * @ingroup Extensions
- * @author Aaron Wright <aaron.wright@gmail.com>
- * @author David Pean <david.pean@gmail.com>
- * @author Jack Phoenix <jack@countervandalism.net>
+ * @author Jacco Geul <jacco@geul.net>
  * @link https://www.mediawiki.org/wiki/Extension:FeaturedUser Documentation
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
@@ -34,113 +34,83 @@ class FeaturedUser {
 	}
 
 	public static function getUser( $input, $args, $parser ) {
-		global $wgMemc, $wgFeaturedUser;
+		global $wgMemc, $wgFeaturedUser, $wgFeaturedUserBits;
 
 		$parser->disableCache();
 
-		$period = ( isset( $args['period'] ) ) ? $args['period'] : '';
-		if ( $period != 'weekly' && $period != 'monthly' ) {
+		if ( !isset( $args['user'] ) ){
 			return '';
 		}
+		$user = $args['user'];
 
-		$user_list = array();
-		$count = 20;
-		$realCount = 10;
+		$bits = array();
+		// If bits are present
+		if ( isset( $args['bits'] ) ){
+			// Put into array
+                        $bits = explode(',',$args['bits']);
+                }
 
-		// Try cache
-		$key = wfMemcKey( 'user_stats', 'top', 'points', 'weekly', $realCount );
-		$data = $wgMemc->get( $key );
+		// Get profile for user
+		$profile = new UserProfile( $user );
+		$profile_data = $profile->getProfile();
+		$profile_data['user_name'] = $user;
 
-		if ( $data != '' ) {
-			wfDebug( "Got top $period users by points ({$count}) from cache\n" );
-			$user_list = $data;
-		} else {
-			wfDebug( "Got top $period users by points ({$count}) from DB\n" );
-
-			$dbr = wfGetDB( DB_SLAVE );
-			$res = $dbr->select(
-				'user_points_' . $period,
-				array( 'up_user_id', 'up_user_name', 'up_points' ),
-				array( 'up_user_id <> 0' ),
-				__METHOD__,
-				array(
-					'ORDER BY' => 'up_points DESC',
-					'LIMIT' => $count
-				)
-			);
-			$loop = 0;
-			foreach ( $res as $row ) {
-				// Prevent blocked users from appearing
-				$user = User::newFromId( $row->up_user_id );
-				if ( !$user->isBlocked() ) {
-					$user_list[] = array(
-						'user_id' => $row->up_user_id,
-						'user_name' => $row->up_user_name,
-						'points' => $row->up_points
-					);
-					$loop++;
-				}
-				if ( $loop >= 10 ) {
-					break;
-				}
-			}
-
-			if ( count( $user_list ) > 0 ) {
-				$wgMemc->set( $key, $user_list, 60 * 60 );
-			}
-		}
-
-		// Make sure we have some data
-		if ( !is_array( $user_list ) || count( $user_list ) == 0 ) {
-			return '';
-		}
-
-		$featured_user = $user_list[array_rand( $user_list, 1 )];
-
-		// Make sure we have a user
-		if ( !$featured_user['user_id'] ) {
-			return '';
-		}
-
-		$output = '<div class="featured-user">';
+		$output = '<div class="featured-user"><table><tr>';
 
 		if ( $wgFeaturedUser['points'] == true ) {
-			$stats = new UserStats( $featured_user['user_id'], $featured_user['user_name'] );
+			$stats = new UserStats( $profile_data['user_id'], $profile_data['user_name'] );
 			$stats_data = $stats->getUserStats();
 			$points = $stats_data['points'];
+			$points = '<br />' . $points;
+		} else {
+			$points = '';
 		}
 
 		if ( $wgFeaturedUser['avatar'] == true ) {
-			$user_title = Title::makeTitle( NS_USER, $featured_user['user_name'] );
-			$avatar = new wAvatar( $featured_user['user_id'], 'ml' );
+			$user_title = Title::makeTitle( NS_USER, $profile_data['user_name'] );
+			$avatar = new wAvatar( $profile_data['user_id'], 'l' );
 			$avatarImage = $avatar->getAvatarURL();
 
-			$output .= "<a href=\"" . htmlspecialchars( $user_title->getFullURL() ) . "\">{$avatarImage}</a>\n";
+			$output .= "<td valign=\"top\"><a href=\"" . htmlspecialchars( $user_title->getFullURL() ) . "\">{$avatarImage}</a></td>\n";
 		}
 
-		$output .= "<div class=\"featured-user-title\">
+		$output .= "<td valign=\"top\"><div class=\"featured-user-title\">
 					<a href=\"" . htmlspecialchars( $user_title->getFullURL() ) . "\">" .
-					wordwrap( $featured_user['user_name'], 12, "<br />\n", true ) .
-					"</a><br /> " .
-				wfMessage( "featured-user-points-{$period}", $points )->text() .
-			"</div>\n\n";
+					"<h3>" . $profile_data['user_name'] . "</h3></a>" . $points . "</div>\n\n";
 
-		if ( $wgFeaturedUser['about'] == true ) {
-			$p = new Parser();
-			$profile = new UserProfile( $featured_user['user_name'] );
-			$profile_data = $profile->getProfile();
-			$about = ( isset( $profile_data['about'] ) ) ? $profile_data['about'] : '';
+		$p = new Parser();
+		$all_keys = array_keys($bits);
+		$last_key = end($all_keys);
+		foreach ($bits as $key => $bit) {
+			// If bit is invalid, skip immidiately
+			if ( !array_key_exists( $bit, $wgFeaturedUserBits )){
+				continue;
+			}
+
+			$label = wfMessage($wgFeaturedUserBits[$bit])->text();
+
+			$text  = ( isset( $profile_data[$bit] ) ) ? $profile_data[$bit] : '';
+
 			// Remove templates
-			$about = preg_replace( '@{{.*?}}@si', '', $about );
-			if ( !empty( $about ) ) {
-				global $wgTitle, $wgOut;
-				$output .= '<div class="featured-user-about-title">' .
-					wfMessage( 'featured-user-about-me' )->text() . '</div>' .
-					$p->parse( $about, $wgTitle, $wgOut->parserOptions(), false )->getText();
+			$text  = preg_replace( '@{{.*?}}@si', '', $text );
+			if ( empty( $text )){
+				continue;
+			}
+			global $wgTitle, $wgOut;
+			if ( $wgFeaturedUser['titles'] == true ) {
+				if ( $wgFeaturedUser['inline'] == true ){
+					$output .= '<b>' . $label . ' </b>';
+				} else {
+					$output .= '<div class="featured-user-title">' . $label . '</div>';
+				}
+			}
+			$output .= $p->parse( $text, $wgTitle, $wgOut->parserOptions(), false )->getText();
+			if ($key != $last_key){
+				$output .= "<br />\n";
 			}
 		}
 
-		$output .= '</div><div class="visualClear"></div>';
+		$output .= '</td></tr></table></div><div class="visualClear"></div>';
 
 		return $output;
 	}
--- FeaturedUser.php
+++ FeaturedUser.php
@@ -1,21 +1,21 @@
 <?php
 /**
  * FeaturedUser extension - adds <featureduser> parser hook
- * to display a featuredly chosen 'featured' user and some info regarding the
- * user, such as their avatar.
+ * to display a chosen user and some info regarding the
+ * user, such as their avatar, bio etc. 
  *
  * Meant to be used with the SocialProfile extension, fails without.
- * Make sure to set either $wgUserStatsTrackWeekly or $wgUserStatsTrackMonthly
- * to true in your wiki's LocalSettings.php and before doing so, be sure that
- * you have created the three necessary tables in the database:
- * user_points_archive, user_points_monthly and user_points_weekly.
- * Then add <featureduser/> tag to whichever page you want to.
+ * Add <featureduser user="User Name"/> tag to display an user on 
+ * any page.
+ *
+ * Heavily inspired by RandomFeaturedUser extension:
+ * https://www.mediawiki.org/wiki/Extension:RandomFeaturedUser 
+ * Aaron Wright <aaron.wright@gmail.com>, David Pean <david.pean@gmail.com>  
+ * and Jack Phoenix <jack@countervandalism.net>.
  *
  * @file
  * @ingroup Extensions
- * @author Aaron Wright <aaron.wright@gmail.com>
- * @author David Pean <david.pean@gmail.com>
- * @author Jack Phoenix <jack@countervandalism.net>
+ * @author Jacco Geul <jacco@geul.net>
  * @link https://www.mediawiki.org/wiki/Extension:FeaturedUser Documentation
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
@@ -23,13 +23,39 @@
 // What to display on the output of <featureduser> tag...
 $wgFeaturedUser['avatar'] = true;
 $wgFeaturedUser['points'] = true;
-$wgFeaturedUser['about'] = true;
+$wgFeaturedUser['titles'] = true;
+$wgFeaturedUser['inline'] = true;
+
+$wgFeaturedUserBits = array();
+$wgFeaturedUserBits['real_name']    = 'user-personal-info-real-name';
+$wgFeaturedUserBits['birthday']     = 'user-personal-info-birthday';
+$wgFeaturedUserBits['occupation']   = 'user-personal-info-occupation';
+$wgFeaturedUserBits['websites']     = 'user-personal-info-websites';
+$wgFeaturedUserBits['places_lived'] = 'user-personal-info-places_lived';
+$wgFeaturedUserBits['schools']      = 'user-personal-info-schools';
+$wgFeaturedUserBits['about']        = 'user-personal-info-about';
+$wgFeaturedUserBits['location']     = 'user-personal-info-location';
+$wgFeaturedUserBits['hometown']     = 'user-personal-info-hometown';
+
+$wgFeaturedUserBits['custom_1']     = 'custom-info-field1';
+$wgFeaturedUserBits['custom_2']     = 'custom-info-field2';
+$wgFeaturedUserBits['custom_3']     = 'custom-info-field3';
+$wgFeaturedUserBits['custom_4']     = 'custom-info-field4';
+
+$wgFeaturedUserBits['movies']       = 'other-info-movies';
+$wgFeaturedUserBits['tv']           = 'other-info-tv';
+$wgFeaturedUserBits['music']        = 'other-info-music';
+$wgFeaturedUserBits['books']        = 'other-info-books';
+$wgFeaturedUserBits['video-games']  = 'other-info-video-games';
+$wgFeaturedUserBits['magazines']    = 'other-info-magazines';
+$wgFeaturedUserBits['snacks']       = 'other-info-snacks';
+$wgFeaturedUserBits['drinks']       = 'other-info-drinks';
 
 // Extension credits that will show up on Special:Version
 $wgExtensionCredits['parserhook'][] = array(
 	'name' => 'FeaturedUser',
-	'version' => '1.3',
-	'author' => array( 'Aaron Wright', 'David Pean', 'Jack Phoenix' ),
+	'version' => '1.0',
+	'author' => array( 'Jacco Geul' ),
 	'descriptionmsg' => 'featureduser-desc',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:FeaturedUser',
 );
@@ -39,14 +65,4 @@ $wgMessagesDirs['FeaturedUser'] = __DIR__ . '/i18n';
 
 $wgAutoloadClasses['FeaturedUser'] = __DIR__ . '/FeaturedUser.class.php';
 
-$wgHooks['ParserFirstCallInit'][] = 'wfFeaturedUser';
-
-/**
- * Set up the <featureduser> tag
- * @param $parser Object: instance of Parser (not necessarily $wgParser)
- * @return Boolean: true
- */
-function wfFeaturedUser( &$parser ) {
-        $parser->setHook( 'featureduser', array( 'FeaturedUser', 'getUser' ) );
-        return true;
-}
+$wgHooks['ParserFirstCallInit'][] = 'FeaturedUser::onParserFirstCallInit';
